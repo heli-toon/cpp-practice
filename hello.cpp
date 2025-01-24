35ca9675b67fb27e31b22ee167266b043b0953c4
Salay Abdul Muhaimin Kanton <abdulkanton2005@gmail.com>
2025-01-24 17:40:01 +0000

    Hello World in C++

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string name;
+
+    cout << "What's your name: ";
+    cin >> name;
+
+    cout << "Hello " << name << endl;
+
+    return 0;
+}
